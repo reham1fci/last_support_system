5d9dfc42cbe1f3e74c6092cc73c2b1978a3b0105
Rehame <reham1fci@yahoo.com>
2022-08-23 02:29:01 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/com/AljazeeraSoft/support_system/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.AljazeeraSoft.support_system
+
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+    }
+}
---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=E:\flutter_versions\flutter1.22.6\flutter"
+export "FLUTTER_APPLICATION_PATH=E:\mac work\Easeus 23 13_17\Code (HFS+)(J)\Existing Files\support_system"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=E:\flutter_versions\flutter1.22.6\flutter\bin\cache\artifacts\engine\ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=3"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
